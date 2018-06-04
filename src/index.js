4cf2050298e2b1c78fb7c91163cc888280e5416f
Mark Venema <markavenema@gmail.com>
2018-06-04 15:19:38 +0900

    Completed React part of course

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Create a new component. THis component should produce some
-// htmls
+
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -12,20 +13,35 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		 };
+		 this.videoSearch('surfboards');
+		
+	}
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
 
 	render() {
+
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
 		return( 
 			<div>
-				<SearchBar />
-				<VideoDetail video={this.state.videos[1]} />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+				onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+				videos={this.state.videos} />
 			</div>
 		);
 	}
